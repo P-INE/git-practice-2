b8947b6c066956c8fe7d2b0ea49c8fe1bb75caec
P-INE <mahomet2illust@gmail.com>
2022-02-19 00:54:30 +0900

    Initial Commit

--- practice/omikuji.js
+++ practice/omikuji.js
@@ -1,7 +1,15 @@
 let username;
 let userresult;
 username = prompt("お名前を教えて下さい。");
-document.getElementById("name").innerHTML = username;
+
+
+Name="名無し"
+if (username == ""){
+  document.getElementById("name").innerHTML=Name
+}else{
+  document.getElementById("name").innerHTML = username;
+}
+
 
 let rand = Math.floor( Math.random() * 5);
 if (rand == 0) {
